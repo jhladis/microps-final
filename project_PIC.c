251c9cdd863b471e2be974c3bca472bfc75cd56d
jhladis <jirka_hladis@hotmail.com>
2014-11-23 22:57:49 -0800

    Update project_PIC.c

--- project_PIC.c
+++ project_PIC.c
@@ -6,19 +6,23 @@
 */
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#include <P32xxxx.h> 
-#include <peripheral/timer.h>
+#include <P32xxxx.h>
+//#include <proc/P32MX675F512H.h>
+//#include <peripheral/timer.h>
+//#include <plib.h>
+
+#define fpga_reset PORTFbits.RF0
+#define fpga_reset_tris TRISFbits.TRISF0
 
 #define clk PORTEbits.RE0 
-#define dataline PORTEbits.RE1 
+#define dataline PORTEbits.RE1
 // Remember: TRIS has 0 for output and 1 for input. 
 #define clk_tris TRISEbits.TRISE0
 #define dataline_tris TRISEbits.TRISE1 
 // define bits for setting open drain mode
 #define clk_odc ODCEbits.ODCE0
 #define dataline_odc ODCEbits.ODCE1
-// one microsecond delay, for Delay10TCYx 
-#define US_DELAY 0.5 
+
 #define MS_RESET 0xff 
 #define MS_DATA_REPORTING_ENABLED 0xf4 
 #define MS_SUCCESS 0xaa 
@@ -26,81 +30,98 @@
 #define MS_ACKNOWLEDGEMENT 0xfa 
 #define COORINFO_XSIGN 0x10 
 #define COORINFO_YSIGN 0x20 
-/* Prototypes */ 
-void wait(unsigned char t); 
-void sendData(unsigned char data); 
-void ps2setup(void);
-unsigned char recvData(void); 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-// global variables
-short ballXpos = 0;
-short ballYpos = 0;
-short ballXvel = 0;
-short ballYvel = 0;
-short pad1Ypos = 0;
-short pad2Ypos = 0;
-short halfpadwidth = 0;
-char score1 = 0;
-char score2 = 0;
-
-unsigned char coorinfo; // Mouse coordinate information 
-unsigned short xpos=0,ypos=0; // Absolute x and y positions. 
-
-// function prototypes
-void initSPI(void);           // initialize SPI link with FPGA
-void spiTXRX(void);           // send ball coords and scores, receive paddle coords
-void playSound(int soundSel); // tell FPGA to play a sound
-void updateBall(void);        // update ball position and velocity
-
-void delay (unsigned int usec);
-//
 
 #define TEN_USEC 55
 #define HUNDRD_USEC 1860
 
+// function prototypes
+void ps2setup(void);          // set up ps2 mouse connection
+void delay (unsigned int usec);
+void sendData(unsigned char data); 
+unsigned char recvData(void); 
+void readMouse(void);         // read amd process data from mouse
+
+void initSPI(void);           // initialize SPI link with FPGA
+void spi_send_receive(unsigned long send1, unsigned long send2);
+
+void sendtoFPGA(void);         // send ball coords and scores, receive paddle coords
+void initTimers(void);        // set up timers
+void playSound(int soundSel); // tell FPGA to play a sound
+void updateBall(void);        // update ball position and velocity
+
+// game parameters
+#define DEFXPOS_BALL 320
+#define DEFYPOS_BALL 320
+#define DEFXVEL_BALL 1
+#define DEFYVEL_BALL 1
+#define UPDATE_PERIOD 800 //30ms refresh period, 20MHz Pb_clk, 1:256 prescaler
+
+// global variables
+long ballXpos = DEFXPOS_BALL; // 10 bits for each position
+long ballYpos = DEFYPOS_BALL;
+long pad1Ypos = 240; // start at midpoint of screen
+long pad2Ypos = 240;
+signed short ballXvel = DEFXVEL_BALL; // initialize to default
+signed short ballYvel = DEFYVEL_BALL;
+short halfpadwidth = 50; //pixel width/2
+short soundSel = 0; // up to 12 bit sound selecor
+short score1 = 0; // 6 bits for score
+short score2 = 0; 
+
+
+unsigned short xpos=0,ypos=0; // Absolute x and y positions. 
+unsigned char coorinfo;
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 void main(void) {
 
-ps2setup();
-//initSPI();
+//ps2setup();
+initSPI();
+initTimers();
 
- while(1) { 
- 	coorinfo = recvData(); 
- 	// The tertiary operator is to determine if we should sign extend 
- 	// the movement data.
-//	xpos += (COORINFO_XSIGN & coorinfo) ? 
-// 	0xFFF0 | recvData() : recvData(); 
- 	ypos += (COORINFO_YSIGN & coorinfo) ? 
- 	0xFFF0 | recvData() : recvData(); 
- 	PORTD = xpos >> 8; // Drive MSB of xpos on PORTD.
- 	
- 	//keep within screen
- 	if (ypos>480) ypos=480;
- 	if (ypos<0) ypos=0;
- 	
- 	
+//TRISD = 0; // PORTD as output, to display x-axis.
+
+ while(1) {
+	//readMouse();
+
+	if (TMR1>=UPDATE_PERIOD){
+    	TMR1=0; // reset timer
+		updateBall(); // update ball position
+	}
+
+	sendtoFPGA();
+ } 
+}
+
+
+void updateBall(void){
  	ballXpos=ballXpos+ballXvel;
  	ballYpos=ballYpos+ballYvel;
- 	if (ballXpos>640) {
+ 	
+	if (ballXpos>640) {
  		ballXpos=640;
  		ballXvel=-1*ballXvel;
  	}
+
  	if (ballXpos<0) {
- 		if (ballYpos<padYpos+halfpadwidth && ballYpos>padYpos-halfpadwidth) {
+ 		//if (ballYpos<pad1Ypos+halfpadwidth && ballYpos>pad1Ypos-halfpadwidth) {
  			// means reflected with paddle
  			ballXpos=0;
  			ballXvel=-1*ballXvel;
- 		} else { //reset ball with default values
- 			// if missed paddle
+ 		/*} else { //reset ball with default values
+ 			// if missed paddle, incement other player's score, reset ball
  			score2++; 
- 			short ballXpos = 0;
- 			short ballYpos = 0;
- 			short ballXvel = 0;
- 			short ballYvel = 0;
- 		}
- 		}
+ 			short ballXpos = DEFXPOS_BALL;
+ 			short ballYpos = DEFYPOS_BALL;
+ 			short ballXvel = DEFXVEL_BALL;
+ 			short ballYvel = DEFYVEL_BALL;
+ 		}*/
  	}
- 	
+	
  	if (ballYpos>480) {
  		ballYpos=480;
  		ballYvel=-1*ballYvel;
@@ -109,12 +130,98 @@ ps2setup();
  		ballYpos=0;
  		ballYvel=-1*ballYvel;
  	}
-	
- } 
+}
+
+void sendtoFPGA(void){ 
+	/*Sending from PIC master to FPGA
+	SPI2: [ballXpos, ballYpos, score1, score2]
+	SPI4: [pad1Ypod, pad2Ypos, (sound select)]*/
+
+	//mask off to prevent overflow
+	unsigned long send1 = (ballXpos<<22)|(ballYpos<<12)|((0x3f&score1)<<6)|(0x3f&score2);
+	unsigned long send2 = (pad1Ypos<<22)|(pad2Ypos<<12)|(0xfff&soundSel);
+	spi_send_receive(send1, send2);
 }
 
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//Timer setup, adapted from class slides
+
+void initTimers(void) {
+// Peripheral clock at 20MHz
+// T1CON
+// bit 15: ON=1: enable timer
+// bit 14: FRZ=0: keep running in exception mode
+// bit 13: SIDL = 0: keep running in idle mode
+// bit 12: TWDIS=1: ignore writes until current write completes
+// bit 11: TWIP=0: don't care in synchronous mode
+// bit 10-8: unused
+// bit 7: TGATE=0: disable gated accumulation
+// bit 6: unused
+// bit 5-4: TCKPS=11: 1:256 prescaler, 0.05us*256=12.8us
+// bit 3: unused
+// bit 2: don't care in internal clock mode
+// bit 1: TCS=0: use internal peripheral clock
+// bit 0: unused
+T1CON = 0b1001000000110000;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//SPI communication, adapted from class slides
+
+void initSPI(void) {
+	long junk;
+	fpga_reset_tris=0;
+	fpga_reset=1; //reset fpga SPI module
+
+	SPI4CONbits.ON = 0; // disable SPI to reset any previous state
+	junk = SPI4BUF; // read SPI buffer to clear the receive buffer
+	SPI4BRG = 0; //set BAUD rate to 10MHz, with Pclk at 20MHz 
+	SPI4CONbits.MSTEN = 1; // enable master mode
+	SPI4CONbits.CKE = 1; // set clock-to-data timing (data centered on rising SCK edge) 
+	SPI4CONbits.MODE32 = 1; //enable 32 bit mode
+	SPI4CONbits.ON = 1; // turn SPI on
+	
+	SPI2CONbits.ON = 0; // disable SPI to reset any previous state
+	junk = SPI2BUF; // read SPI buffer to clear the receive buffer
+	SPI2BRG = 0; //set BAUD rate to 10MHz, with Pclk at 20MHz 
+	SPI2CONbits.MSTEN = 1; // enable master mode
+	SPI2CONbits.CKE = 1; // set clock-to-data timing (data centered on rising SCK edge) 
+	SPI2CONbits.MODE32 = 1; //enable 32 bit mode
+	SPI2CONbits.ON = 1; // turn SPI on
+
+	fpga_reset=0;
+}
+
+void spi_send_receive(unsigned long send1, unsigned long send2) {
+	SPI2BUF = send1; // send data to slave
+	while (SPI2STATbits.SPIBUSY); // wait until received buffer fills, indicating data received 
+
+	SPI4BUF = send2; // send data to slave
+	while (SPI4STATbits.SPIBUSY); // wait until received buffer fills, indicating data received 
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// PS2 communication, adapted from "MicroToys Guide: PS/2 Mouse N. Pinckney April 2005"
+
+void readMouse(void){
+	coorinfo = recvData(); // Mouse coordinate information 
+ 	// The tertiary operator is to determine if we should sign extend 
+ 	// the movement data.
+	xpos += (COORINFO_XSIGN & coorinfo) ? 
+    0xFFF0 | recvData() : recvData(); 
+ 	pad1Ypos += (COORINFO_YSIGN & coorinfo) ? 
+ 	0xFFF0 | recvData() : recvData();
+ 	
+ 	//keep within screen
+ 	if (pad1Ypos>480) pad1Ypos=480;
+ 	if (pad1Ypos<0) pad1Ypos=0;
+	PORTD = xpos;
+}
+
 void sendData(unsigned char data) { 
  char i; 
  unsigned char parity = 1; 
@@ -147,7 +254,6 @@ void sendData(unsigned char data) {
  // (both go high.) 
 }
 
- 
 unsigned char recvData(void) { 
  unsigned char data = 0x0; 
  unsigned char parity; 
@@ -173,9 +279,7 @@ unsigned char recvData(void) {
  return data; 
 } 
 
-
 void ps2setup(void) { 
- TRISD = 0; // PORTD as output, to display x-axis.
  clk_odc = 1; // enable open drain output on clock pin
  dataline_odc = 1; //  enable open drain output on data pin
  //clk_tris = 1; // Clock as input. (Open collector) 
@@ -193,7 +297,6 @@ while(recvData() != MS_DEVICE_ID); // Wait for mouse device id to be
 // mode. 
 }
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 void delay (unsigned int usec) {
  unsigned int start, stop;
 
